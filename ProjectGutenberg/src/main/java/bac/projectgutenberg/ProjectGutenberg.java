bc10f5797ca7df8039deeb341b5b72dc32aa4f1c
buszlaiadam <50669718+buszlaiadam@users.noreply.github.com>
2022-12-27 14:49:28 +0200

    Add files via upload

---
+++ ProjectGutenberg/src/main/java/bac/projectgutenberg/ProjectGutenberg.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package bac.projectgutenberg;
+
+/**
+ *
+ * @author adam
+ */
+public class ProjectGutenberg {
+
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts), ");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and ");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg, ");
+        System.out.println("printing whatever we want to the console with ease.");
+    }
+}
