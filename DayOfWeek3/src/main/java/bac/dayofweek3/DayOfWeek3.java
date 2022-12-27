e6bcc30ee1d5769d1c72d045b236e2939cfd8871
buszlaiadam <50669718+buszlaiadam@users.noreply.github.com>
2022-12-27 14:44:24 +0200

    Add files via upload

---
+++ DayOfWeek3/src/main/java/bac/dayofweek3/DayOfWeek3.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package bac.dayofweek3;
+
+/**
+ *
+ * @author adam
+ */
+public class DayOfWeek3 {
+
+    public static void main(String[] args) {
+        int day = 4;
+        String dayName = "";
+        
+        switch (day) {
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        case 7:
+            dayName = "Sunday";
+            break;
+        default:
+            dayName = "Invalid day";
+    }
+    
+        System.out.println("The day is " + dayName);
+    }
+}
