a521446bbf8bf9deef01532c64a096d22f9b7880
buszlaiadam <50669718+buszlaiadam@users.noreply.github.com>
2022-12-27 14:46:44 +0200

    Add files via upload

---
+++ HelloWorld/src/main/java/bac/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package bac.helloworld;
+
+/**
+ *
+ * @author adam
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Harry!");
+    }
+}
